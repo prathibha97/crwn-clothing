2b3f988c17dea3240cae833495df907f6c33db62
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-18 15:08:27 +0530

    Setup fireStore to store users

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,18 +1,18 @@
-import {signInWithGooglePopup} from '../../utils/firebase'
+import { createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase'
 
 const SignIn = () => {
 
-    const logGoogleUser = async ()=>{
-        const response = await signInWithGooglePopup()
-        console.log(response)
-    }
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup()
+    const userDocRef = await createUserDocumentFromAuth(user)
+  }
 
   return (
     <div>
-        <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>
-            Sign in with Google
-        </button>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>
+        Sign in with Google
+      </button>
     </div>
   )
 }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,7 @@
 import {initializeApp} from 'firebase/app'
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
 import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDg8sCgODpRjBEfUF3uLV4m8XNZIzPihdk",
@@ -19,3 +21,26 @@ provider.setCustomParameters({
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    const userSnapShot = await getDoc(userDocRef)
+
+    if(!userSnapShot.exists()){
+        const {displayName, email} = userAuth
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef,{
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (err) {
+            console.log('error creating the user', err.message)
+        }
+    }
+return userDocRef
+}
