dfd8e12f798244d744c59c71c32b9ec4b7d801ef
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-22 10:22:37 +0530

    Cartdropdown style fix

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, CartItem } from '../../components'
 import { CartContext } from '../../context/cart.context'
-import { CartDropdownContainer, CartItems } from './CartDropdown.styles'
+import { CartDropdownContainer, CartItems, EmptyMessage } from './CartDropdown.styles'
 
 const CartDropdown = () => {
   const navigate = useNavigate()
@@ -14,9 +14,11 @@ const CartDropdown = () => {
   return (
     <CartDropdownContainer>
       <CartItems>
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
       </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </CartDropdownContainer>
