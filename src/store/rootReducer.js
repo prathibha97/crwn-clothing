93e7f725a9bfc2b51d0fe108df2bc1a0683242ae
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-25 08:55:37 +0530

    Add Redux Persist

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
-
 import { cartReducer } from './cart/cart.reducer'
 import { categoriesReducer } from './categories/category.reducer'
 import { userReducer } from './users/user.reducer'
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,22 @@
 import { applyMiddleware, compose, legacy_createStore } from 'redux'
 import logger from 'redux-logger'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { rootReducer } from './rootReducer'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleware = [logger]
 
 const composedEnhancers = compose(applyMiddleware(...middleware))
 
-export const store = legacy_createStore(rootReducer, undefined, composedEnhancers)
+export const store = legacy_createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store)
 
