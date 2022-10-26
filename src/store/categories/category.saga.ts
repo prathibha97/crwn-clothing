e5503909c8d7bd191d7a1ac5f10f7590cb652ad2
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-26 18:51:23 +0530

    Type category saga

---
+++ src/store/categories/category.saga.ts
@@ -0,0 +1,30 @@
+import { all, call, put, takeLatest } from 'typed-redux-saga';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase';
+import {
+  fetchCategoriesFailed,
+  fetchCategoriesSuccess,
+} from './category.action';
+import { CATEGORY_ACTION_TYPES } from './category.types';
+
+export function* fetchCategoriesAsync() {
+  try {
+    const categoriesArray = yield* call(
+      getCategoriesAndDocuments,
+      'categories'
+    );
+    yield* put(fetchCategoriesSuccess(categoriesArray));
+  } catch (err) {
+    yield* put(fetchCategoriesFailed(err as Error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield* takeLatest(
+    CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START,
+    fetchCategoriesAsync
+  );
+}
+
+export function* categoriesSaga() {
+  yield* all([call(onFetchCategories)]);
+}
